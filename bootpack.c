191a22ea4fcd561001b19b9d853166fa36200e83
mnsfhxy <mnsfhxy@163.com>
2021-09-30 02:50:11 +0800

    day04

--- bootpack.c
+++ bootpack.c
@@ -1,15 +1,21 @@
 /* 告诉C编译器，有一个函数在别的文件里 */
 
 void io_hlt(void);
-
+void write_mem8(int addr,int data);
 /* 是函数声明却不用{}，而用;，这表示的意思是：
 	函数在别的文件中，你自己找一下 */
 
 void HariMain(void)
 {
 
-fin:
-	io_hlt(); /* 执行naskfunc.nas中的_io_hlt函数 */
-	goto fin;
-
+	int i;
+	char *p;
+	for(i=0xa0000;i<=0xaffff;i++){
+		p=(char *)i;
+		*p=i&0x0f;
+	}
+	for(;;){
+		io_hlt();
+	}
+		
 }
