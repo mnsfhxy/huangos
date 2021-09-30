464e69dd47c2641feb33dd86ab745759f50b06e7
mnsfhxy <mnsfhxy@163.com>
2021-10-01 02:01:51 +0800

    day05

--- bootpack.c
+++ bootpack.c
@@ -36,17 +36,24 @@ void init_screen(char *vram,short xsize,short ysize);
 void set_palette(int start,int end,unsigned char *rgb);
 int io_load_eflags(void);//eflags ---- extend flags 保存中断标志
 void io_store_eflags(int eflags);//加载中断标志
-void print_font(char *vram,int xsize,int x,int y,char c,char *font);//8*16像素阵绘制  font是要绘制的矩阵
-void boxfill8(unsigned char *vram, int xsize,unsigned char c,int x0,int y0,int x1,int y1);//矩形绘制
+void printFont(char *vram,int xsize,int x,int y,char c,char *font);//绘制字体,8*16像素阵绘制  font是要绘制的矩阵
+void printString(char *vram,int xsize,int x,int y,char c,unsigned char *s);//输出字符串
+void boxfill(unsigned char *vram, int xsize,unsigned char c,int x0,int y0,int x1,int y1);//矩形绘制
+void drawPicture(char *vram,int vxsize,int pic_x_size,int pic_y_size,int pic_x,int pic_y,char *pic_addr,int bxsize);
+void init_mouse_cursor(char *mouse, char bc);
 void HariMain(void){
 		BOOTINFO *bootinfo=(BOOTINFO *)0x0ff0;
 		init_palette();
 		init_screen(bootinfo->vram,bootinfo->screen_x,bootinfo->screen_y);
-		static char font_A[16] = {
-			0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
-			0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
-			};
-		print_font(bootinfo->vram,bootinfo->screen_x,5,5,COL8_FF00FF,font_A);
+		char *s;
+		sprintf(s,"screen_x=%d",bootinfo->screen_x);
+		printString(bootinfo->vram,bootinfo->screen_x,5,5,COL8_FF00FF,s);
+		char cursor[16][16];
+		int cursor_x,cursor_y;
+		cursor_x=160;
+		cursor_y=100;
+		init_mouse_cursor(cursor,COL8_008484);
+		drawPicture(bootinfo->vram,bootinfo->screen_x,16,16,cursor_x,cursor_y,cursor,16);
 		for(;;)io_hlt();
 }
 void init_palette(void){
@@ -76,22 +83,22 @@ void init_palette(void){
 void init_screen(char *vram,short xsize,short ysize){
 	
 	
-	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
-	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
-	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
-	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+	boxfill(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
 
-	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
-	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
-	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
-	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
-	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
-	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+	boxfill(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
 
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	boxfill(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 
 }	
 void set_palette(int start,int end,unsigned char *rgb){
@@ -108,7 +115,7 @@ void set_palette(int start,int end,unsigned char *rgb){
 		io_store_eflags(eflags);
 		return;
 	}
-void boxfill8(unsigned char *vram, int xsize,unsigned char c,int x0,int y0,int x1,int y1){
+void boxfill(unsigned char *vram, int xsize,unsigned char c,int x0,int y0,int x1,int y1){
 	int x,y;
 	for(y=y0;y<=y1;y++){
 		for(x=x0;x<=x1;x++)
@@ -116,21 +123,79 @@ void boxfill8(unsigned char *vram, int xsize,unsigned char c,int x0,int y0,int x
 	}
 	return;
 }
-void print_font(char *vram,int xsize,int x,int y,char c,char *font){
+void printFont(char *vram,int xsize,int x,int y,char c,char *font){
 	int i,j;
+	unsigned char k[8]={
+		0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01	
+	};
 	for(i=0;i<=15;i++){
-		unsigned char *k;
-		*k=0x80;
 		for(j=0;j<=7;j++){
-			if((font[i]&(*k))!=0){
+			if((font[i]&(k[j]))!=0){
 				*(vram+x+(i+y)*xsize+j)=c;
 			}
-			*k=*k>>1;
-		}
-		
+		}	
 	}
 		
 	return;
 }
+void printString(char *vram,int xsize,int x,int y,char c,unsigned char *s){
 	
+	extern char hankaku[4096];
+	 for(;*s!=0x00;s++){
+		printFont(vram,xsize,x,y,c,hankaku+(*s)*16);
+		x=x+8;
+	} 
+	/* printFont(vram,xsize,x,y,c,hankaku+(*s)*16);
+	printFont(vram,xsize,x+8,y,c,hankaku+(*s+1)*16);
+	printFont(vram,xsize,x+16,y,c,hankaku+(*s+2)*16);
+	printFont(vram,xsize,x+24,y,c,hankaku+(*s+3)*16); */
+	return;
 	
+}	
+void drawPicture(char *vram,int vxsize,int pic_x_size,int pic_y_size,int pic_x,int pic_y,char *pic_addr,int bxsize){	
+	int x,y;
+	for(y=0;y<pic_y_size;y++){
+		
+		for(x=0;x<pic_x_size;x++)
+			*(vram+(y+pic_y)*vxsize+pic_x+x)=*(pic_addr+y*bxsize+x);
+	
+		
+	}
+	return;
+}
+void init_mouse_cursor(char *mouse, char bc){
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
